e934c65245d090a8b123009fc851c2990c29100c
Christie Molloy <cmolloy@redhat.com>
2020-08-21 15:41:54 -0400

    add all files

---
+++ src/app/MainPage/MainPage.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { PageSection, Title } from '@patternfly/react-core';
+
+const MainPage: React.FunctionComponent = () => (
+  <PageSection>
+    <Title headingLevel="h1" size="lg">This is where SSO would go.</Title>
+  </PageSection>
+)
+
+export { MainPage };
