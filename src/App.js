63cacd8ae4e50872c28125995e47644590a105de
redallen <zallen@redhat.com>
2020-09-01 15:29:51 -0400

    contextual side nav

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
 import { HashRouter, Route, Switch } from "react-router-dom";
 
 import "@patternfly/react-core/dist/styles/base.css";
-import TopNav from "./components/TopNav";
 import React from "react";
 import localRoutes from "./routes";
 import remoteRoutes from "threeScale/routes";
-import { Sidebar } from './components/Sidebar';
-import { Page } from '@patternfly/react-core';
 
 const routes = [...localRoutes, ...remoteRoutes];
 
 const App = () => (
   <HashRouter>
-    <Page header={<TopNav />} sidebar={<Sidebar />} isManagedSidebar>
-      <React.Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          {routes.map((route) => (
-            <Route
-              key={route.path}
-              path={route.path}
-              component={route.component}
-              exact={route.exact}
-            />
-          ))}
-        </Switch>
-      </React.Suspense>
-    </Page>
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        {routes.map(({ path, component, exact }) => (
+          <Route
+            key={path}
+            path={path}
+            component={component}
+            exact={exact}
+          />
+        ))}
+      </Switch>
+    </React.Suspense>
   </HashRouter>
 );
 
